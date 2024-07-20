d7056369765c2972318dfbc41e74bcf5e770829d
typhoooon <zhifeng23333@gmail.com>
2024-07-21 00:06:43 +0800

    UPDATE

--- script.js
+++ script.js
@@ -1,21 +1,52 @@
-async function checkWord(){
-    const wordInput = document.getElementById('wordInput').value.toLowerCase();
-    const result = document.getElementById('result');
+let currentIndex=0
+let wordsData = [];
+let wordsKeys = [];
+loading();
+displayWord();
 
+async function loading() {
     try{
         const response = await fetch('words.json');
         const data = await response.json();
-        const words = data.words;
-
-        if(words[wordInput]){
-            result.textContent = `${wordInput}} 正确`;
-            result.style.color='green';
-        }
-        else{
-            result.textContent = `${wordInput}} 错误`;
-            result.style.color='red';
-        }
+        wordsData = data.words;
+        wordsKeys = Object,keys(wordsData);
+        result.textContent='loading successfully'
     }catch(error){
         result.textContent='loading failed'
+    }
+}
+
+function displayWord() {
+    const wordDisplay = document.getElementById('wordDisplay');
+    const currentWordKey = wordsKeys[currentIndex];
+    wordDisplay.textContent=wordsData[currentWordKey];
+}
+
+function checkWord(){
+    const wordInput = document.getElementById('wordInput').value.toLowerCase();
+    const result = document.getElementById('result');
+    const currentWordKey = wordsKeys[currentIndex];
+
+    if(wordInput===currentWordKey){
+        result.textContent = `${wordInput} 正确`;
+        result.style.color='green';
+    }
+    else{
+        result.textContent = `${wordInput} 错误`;
+        result.style.color='red';
+    }
+}
+
+function nextWord(){
+    if(currentIndex < wordsKeys.length - 1){
+        currentIndex++;
+        displayWord();
+    }
+}
+
+function prevWord(){
+    if(currentIndex > 0){
+        currentIndex--;
+        displayWord();
     }
 }
